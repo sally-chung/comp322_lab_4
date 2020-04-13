91ccb94f8e52f1b1bb6b2a79828018a9ce167e64
sally-chung <sally.wchung@gmail.com>
2020-04-13 02:00:37 -0700

    created sig handler for SIGTERM

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # comp322_lab_4
 Rice, rice, and rice.
+
+4/03 - README and dining-p
+4/05 - created eat() and think() functions/skele
+4/13 - initialized signal_handler for SIGTERM
--- dining-p.c
+++ dining-p.c
@@ -8,35 +8,71 @@
 #include <signal.h>
 #include <semaphore.h>
 #include <time.h>
+#include <stdbool.h>
 
-void eat();
-void think();
+void eat(int n);
+void think(int n);
+void signal_handler(int signum);
 
 int main(int argc, char* argv[]) {
     sem_t chopstick[5];
     unsigned int usecs;
-    
+    int n, m;
+    n = 0;
+    m = 0;
+
+    int i;
+    i = 0;
+
     usecs = rand();
 
+    signal(SIGTERM, signal_handler);   
+
+    while(1) {
+        printf("Processing things #%d.\n", i);
+        i++;
+        sleep(1);
+    }
+
+/*
     do { 
-        wait(chopstick[i]);
-        wait(chopstick[(i+1)]);
+        // wait(chopstick[i]);
+        // wait(chopstick[(i+1)]);
         /* ...
-        eat for awhile */
-        signal(chopstick[i]);
-        signal(chopstick[(i+1)]);
+        eat for awhile
+        // signal(SIGTERM, eat)
+        
+        printf("Sleeping now..\n");
+        usleep(random);
+        signal(SIGTERM, signal_handler);
+        //signal(chopstick[i], think);
+        //signal(chopstick[(i+1)], think);
         /* ...
-        think for awhile */
+        think for awhile
     } while(true);
+    */
+
+    return EXIT_SUCCESS;
 
 }
 
-void eat(unsigned int usecs){
-    usleep(usecs);
-    printf("Philosopher #%d is eating", number);
+// signal handler for SIGTERM when cmd 'kill -TERM pid' is called
+void signal_handler(int signum) {
+    fprintf(stderr, "Philosopher #n completed m cycles\n");
+    printf("Caught signal %d\n", signum);
+    exit(signum);
 }
 
-void think(unsigned int usecs){
-    usleep(usecs);
-    printf("Philosopher #%d is thinking", number);
+void eat(int n){
+    printf("Philosopher #%d is eating", n);
+    exit(1);
 }
+
+void think(int n){
+    printf("Philosopher #%d is thinking", n);
+    exit(1);
+}
+
+
+// ps -aux | grep dining-p
+// kill -TERM 'pid'
